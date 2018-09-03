13c6c647a01ef2df007cc2c297b966826e9761d5
竹中寛宰 <hirotada@takenakahiroshisainomacbook-pro.local>
2018-09-03 15:26:20 +0900

    Initial Commit

---
+++ PrizeCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PrizeCalculator
+//
+//  Created by 竹中寛宰 on 2018/09/03.
+//  Copyright © 2018年 SocialNeeds. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
