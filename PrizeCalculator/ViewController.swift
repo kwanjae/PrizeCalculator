6236a1fceaed46c002a40382ceb978db7c6cab75
Kwanjae Kim <kanje.kim@gmail.com>
2018-09-05 16:20:35 +0900

    メイン機能作成

--- PrizeCalculator/ViewController.swift
+++ PrizeCalculator/ViewController.swift
@@ -9,7 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var lStepper: UIStepper!
+    @IBOutlet weak var mStepper: UIStepper!
+    @IBOutlet weak var sStepper: UIStepper!
+    
+    @IBOutlet weak var lLabel: UILabel!
+    @IBOutlet weak var mLabel: UILabel!
+    @IBOutlet weak var sLabel: UILabel!
+    @IBOutlet weak var answerLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +28,33 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    @IBAction func lStepperChanged(_ sender: UIStepper) {
+        let num = Int(sender.value)
+        lLabel.text = String(num)
+        
+        calculatePrize()
+    }
+    @IBAction func mStepperChanged(_ sender: UIStepper) {
+        let num = Int(sender.value)
+        mLabel.text = String(num)
+        
+        calculatePrize()
+    }
+    @IBAction func sStepperChanged(_ sender: UIStepper) {
+        let num = Int(sender.value)
+        sLabel.text = String(num)
+        
+        calculatePrize()
+    }
+    
+    private func calculatePrize() {
+        let lnum = Int(lStepper.value)
+        let mnum = Int(mStepper.value)
+        let snum = Int(sStepper.value)
+        
+        let answerNum = (lnum*5500) + (mnum*1500) + (snum*1000)
+        
+        answerLabel.text = String(answerNum)
+    }
 }
 
